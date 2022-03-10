cc675ba7e4c5b4f935a9ce77788345eb3d8fd00e
Patil <Patil@LAPTOP-J7DCG374>
2022-03-10 18:57:53 +0530

    first commit

---
+++ java1/src/basic/Datatype.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		int num = 5;
+		float f = 1.99f;
+		char c = 'd';
+		boolean b = true;
+		String s = "HELLO";
+		
+		System.out.println(num);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+	}
+
+}
